23d8e232bf238f5385323161eac45aeb0a295f3b
Aliencryptodev <aliencryptodev@gmail.com>
2025-05-19 15:04:11 +0200

    Update Project Configuration

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://217.65.144.64:3000',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
+});
