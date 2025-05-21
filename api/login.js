69c4378242f6010baa76ffb16027dac6a2dfec31
Aliencryptodev <aliencryptodev@gmail.com>
2025-05-22 01:34:01 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,6 +3,8 @@ const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const REDIRECT_URI = process.env.DISCORD_REDIRECT_URI; // Esta será la URL de Vercel
 
 module.exports = (req, res) => {
+  // Asegúrate de que los backticks `` se usan para la plantilla de string
+  // Y que las variables se insertan con ${}
   const url = `https://discord.com/api/oauth2/authorize?client_id=<span class="math-inline">\{CLIENT\_ID\}&redirect\_uri\=</span>{encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify guilds guilds.members.read`;
   res.redirect(url);
 };
